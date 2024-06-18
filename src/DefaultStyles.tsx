30ff523f396a817c2b4f957de7cc40604827adf4
Albinoni <albinoni1423@gmail.com>
2024-06-18 11:28:21 +0400

    Default Styles added

---
+++ src/DefaultStyles.tsx
@@ -0,0 +1,21 @@
+import { Platform, StyleSheet } from "react-native";
+
+export const DefaultStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  Title: {
+    fontWeight: "600",
+    fontSize: 30,
+    color: "#fff"
+  },
+  Text: {
+    fontWeight: "300",
+    fontSize: 20,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Arial",
+    color: "#fff",
+    textAlign: "center"
+  }
+});
