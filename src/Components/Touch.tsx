7f26dc6aa25ac23de36c5877814268973bafc4d5
Albinoni <albinoni1423@gmail.com>
2024-06-18 11:24:21 +0400

    Custom Button created

---
+++ src/Components/Touch.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  style: Object;
+  onPress: () => void;
+};
+
+const Touch = (props: Props) => {
+  return (
+    <TouchableOpacity onPress={props.onPress}>
+      <Text style={[props.style, styles.Padd]}>{props.children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Touch;
+
+const styles = StyleSheet.create({
+  Padd: {
+    paddingHorizontal: 10,
+    paddingVertical: 10
+  }
+});
